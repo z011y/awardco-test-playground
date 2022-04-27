c1256dfc06ab5e497a918333c378630b842c16f8
jcjen62 <jcjensen62@gmail.com>
2022-04-27 14:48:25 -0600

    Part 1

--- src/components/FakerComponent.vue
+++ src/components/FakerComponent.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+    <button class="button" v-on:click="getFaker">Get Data</button>
     <div id="containerData" v-for="user in fake" :key="user.id">
         <p>{{ user.name }}</p>
         <p><i>{{ user.catchPhrase}}</i></p>
@@ -17,13 +18,48 @@ export default {
             fake: null
         }
     },
-    mounted () {
-        axios
-            .get('https://jsonplaceholder.typicode.com/users/1')
-            .then(response => (this.fake = response.data))
-    },
+    methods: {
+        getFaker: function () {
+            axios
+                .get('https://jsonplaceholder.typicode.com/users/1')
+                .then(response => (this.fake = response.data))
+            
+        }
+    }
 }
 </script>
 
 <style scoped>
+  .component-container[data-v-314b4107] {
+    padding-top: 0;
+  }
+    .button {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding: 16px;
+
+    height: 40px;
+    color: white;
+    font-size: 16px;
+
+    background: #111827;
+    border-radius: 20px;
+    transition: 0.25s ease-in-out;
+    border: none;
+  }
+
+  .button:hover {
+    cursor: pointer;
+    background: #000;
+  }
+
+  .button:disabled {
+    opacity: 0.4;
+  }
+
+  .button:disabled:hover {
+    cursor: not-allowed;
+  }
 </style>
--- src/views/Faker.vue
+++ src/views/Faker.vue
@@ -1,38 +1,37 @@
 <template>
   <div class="container">
-      <back-button />
-      <component-layout
-        title="Faker"
-      >
-          <faker-component />
-      </component-layout>
+    <back-button />
+    <component-layout
+      title="Faker"
+    >
+      <faker-component />
+    </component-layout>
   </div>
 </template>
 
 <script>
 import BackButton from "../components/BackButton.vue";
 import ComponentLayout from "../components/ComponentLayout.vue";
-import FakerComponent from '../components/FakerComponent.vue';
+import FakerComponent from "../components/FakerComponent.vue";
 
 export default {
-    name: "Faker",
-    components: {
-      BackButton,
-      ComponentLayout,
-      FakerComponent,
-    },
-
-}
+  name: "Faker",
+  components: {
+    BackButton,
+    ComponentLayout,
+    FakerComponent,
+  },
+};
 </script>
 
 <style scoped>
-    .container {
-    display: flex;
-    flex-direction: column;
-    width: 100vw;
-    height: calc(100vh - 72px);
-    justify-content: center;
-    align-items: center;
-    padding: 48px;
-  }
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: calc(100vh - 72px);
+  justify-content: center;
+  align-items: center;
+  padding: 48px;
+}
 </style>
